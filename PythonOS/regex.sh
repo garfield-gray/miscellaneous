10109e15646510ce6ed637de2e377e68d2e957c2
g <garfield.gray.999@gmial.com>
2025-05-08 04:29:54 -0400

    ho

---
+++ PythonOS/regex.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+ 
+echo grep -i python /usr/share/dict/words:
+echo 
+grep -i python /usr/share/dict/words
+echo
+
+
+
+echo grep  f.c.te /usr/share/dict/words:
+echo 
+grep  f.c.te /usr/share/dict/words
+echo
+
+
+echo grep -i raction$ /usr/share/dict/words:
+echo
+grep -i raction$ /usr/share/dict/words
+echo
+
+echo grep -i ^fect /usr/share/dict/words:
+echo 
+grep -i ^fect /usr/share/dict/words
+echo
