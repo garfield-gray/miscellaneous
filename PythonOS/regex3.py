921b95657db5c446d0683216a434fa36970cd181
g <garfield.gray.999@gmial.com>
2025-05-15 13:11:35 -0400

    end of regex

---
+++ PythonOS/regex3.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+
+
+import re
+
+print(re.split(r"[.?!]", "One sentence. Another one? And the last one!"))
+
+print(re.split(r"([.?!])", "One sentence. Another one? And the last one!"))
+
+
+
+print(re.sub(r"[\w.%+-]+@[\w.-]+", "[REDACTED]", "Received an email for go_nuts95@my.example.com"))
+
+print(re.sub(r"^([\w .-]*), ([\w .-]*)$", r"\2 \1", "Lovelace, Ada"))
