f78bf63f00c3fad5ca413ea998c4098fbe21e0ae
g <garfield.gray.999@gmial.com>
2025-05-24 08:27:53 -0400

    bash script to move the files

--- polygons/organize_kml.sh
+++ polygons/organize_kml.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 # Go to the directory containing the .kml files
-cd /path/to/your/kml/files || exit 1
+cd kmlfiles/
 
 # Loop over each .kml file
 for file in *.kml; do
   # Use regex to extract the base 10-digit number at the start
   if [[ "$file" =~ ^([0-9]{10}) ]]; then
     base="${BASH_REMATCH[1]}"
-    
+    echo "${BASH_REMATCH[1]}"
+
     # Make directory named after the base if it doesn't exist
     mkdir -p "$base"
     
     # Move the file into the correct folder
-    mv "$file" "$base/"
+    # mv "$file" "$base/"
   fi
 done
 
