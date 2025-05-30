16655ec5db2f2409f6e37e581b34d330341c5ed3
g <garfield.gray.999@gmial.com>
2025-05-30 08:04:07 -0400

    some improvements

--- PythonOS/testing/rearrange_test.py
+++ PythonOS/testing/rearrange_test.py
@@ -17,6 +17,20 @@ class TestRearrange(unittest.TestCase):
         testcase = ""
         expected = ""
         self.assertEqual(rearrange_name(testcase), expected)
+
+
+
+    def test_double_name(self):
+        testcase = "Hopper, Grace M."
+        expected = "Grace M. Hopper"
+        self.assertEqual(rearrange_name(testcase), expected)
+
+    def test_one_name(self):
+        testcase = "Voltaire"
+        expected = "Voltaire"
+        self.assertEqual(rearrange_name(testcase), expected)
+
+
 # Run the tests
 unittest.main()
 
